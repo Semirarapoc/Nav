2dd236c94c828d87e888256177db80e83313663a
Semirarapoc <121005323+Semirarapoc@users.noreply.github.com>
2023-01-12 18:16:40 +0800

    Delete EXCEL_SUCCESS_EXECUTION directory

--- EXCEL_SUCCESS_EXECUTION/Res1/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\ccdesphy\\Documents\\GitHub\\DevOps\\EXCEL_SUCCESS_EXECUTION\\Res1',namePairs:[]}]};
